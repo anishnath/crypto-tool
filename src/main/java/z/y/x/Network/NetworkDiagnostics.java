c78ab669c028dea58e6a505236bb0b8210f0b55b
aninath <aninath@171.78.197.124>
2014-02-25 21:53:15 +0530

    Fixed the Code

--- src/main/java/z/y/x/Network/NetworkDiagnostics.java
+++ src/main/java/z/y/x/Network/NetworkDiagnostics.java
@@ -19,7 +19,6 @@ public class NetworkDiagnostics {
 			else
 				traceRt = Runtime.getRuntime().exec(
 						"traceroute " + address.getHostAddress());
-			
 
 		} catch (Exception e) {
 			System.out.println(e);
@@ -27,34 +26,42 @@ public class NetworkDiagnostics {
 
 		return route;
 	}
-	
-	public static String doCommand(List<String> command) 
-			  throws IOException
-			  {
-			    String s = null;
 
-			    StringBuilder builder = new StringBuilder();
-			    ProcessBuilder pb = new ProcessBuilder(command);
-			    Process process = pb.start();
+	public static String doCommand(List<String> command)
 
-			    BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			    BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+	{
+		StringBuilder builder = new StringBuilder();
+		try {
+			String s = null;
 
-			    // read the output from the command
-			    System.out.println("Here is the standard output of the command:\n");
-			    while ((s = stdInput.readLine()) != null)
-			    {
-			     builder.append(s +"<br>");
-			     builder.append(System.getProperty("line.separator"));
-			    }
+			ProcessBuilder pb = new ProcessBuilder(command);
+			Process process = pb.start();
 
-			    // read any errors from the attempted command
-			    System.out.println("Here is the standard error of the command (if any):\n");
-			    while ((s = stdError.readLine()) != null)
-			    {
-			    	builder.append(s);
-			    	 builder.append(System.getProperty("line.separator"));
-			    }
-			  return  builder.toString();
-			  }
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(
+					process.getInputStream()));
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(
+					process.getErrorStream()));
+
+			// read the output from the command
+			System.out.println("Here is the standard output of the command:\n"
+					+ command.toString());
+			while ((s = stdInput.readLine()) != null) {
+				builder.append(s + "<br>");
+				builder.append(System.getProperty("line.separator"));
+			}
+
+			// read any errors from the attempted command
+			System.out
+					.println("Here is the standard error of the command (if any):\n");
+			while ((s = stdError.readLine()) != null) {
+				builder.append(s + "<br>");
+				builder.append(System.getProperty("line.separator"));
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return builder.toString();
+	}
 }
