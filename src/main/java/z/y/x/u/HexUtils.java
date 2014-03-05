9fd407793b31fb15d9c963cef9074d8664a39f74
aninath <aninath@223.187.194.42>
2014-03-05 23:47:33 +0530

    Added Hex UTILS CLASS

---
+++ src/main/java/z/y/x/u/HexUtils.java
@@ -0,0 +1,34 @@
+package z.y.x.u;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+/**
+ * 
+ * @author ANish
+ *
+ */
+public class HexUtils {
+	
+	public static Object decode(final String string) throws DecoderException
+	{
+		return (Object)new Hex().decode(string);
+		
+	}
+	
+	public static String encodeHex(byte[] by)
+	{
+	      char[]  actual = Hex.encodeHex(by);
+	     return "";
+	}
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println(decode("06"));
+		} catch (DecoderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
