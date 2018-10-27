e833141f1692d4293874f40c474567a564009580
aninath <aninath@cisco.com>
2018-10-27 14:38:05 +0530

    Added New Functionality

--- src/main/java/z/y/x/Security/RSAFunctionality.java
+++ src/main/java/z/y/x/Security/RSAFunctionality.java
@@ -32,6 +32,7 @@ public class RSAFunctionality extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     private static final String METHOD_CALCULATERSA = "CALCULATE_RSA";
+    private static final String METHOD_SIGN_VERIFY_TERSA = "RSA_SIGN_VERIFY_MESSAGEE";
 
 
 
@@ -53,6 +54,7 @@ public class RSAFunctionality extends HttpServlet {
 
 
         String keysize = request.getParameter("keysize");
+        String pageName=request.getParameter("rsasignverifyfunctions");
         if (keysize != null && keysize.trim().length() > 0) {
             try {
 
@@ -89,7 +91,15 @@ public class RSAFunctionality extends HttpServlet {
                 request.getSession().setAttribute("pubkey", pgppojo.getPubliceKey());
                 request.getSession().setAttribute("privKey", pgppojo.getPrivateKey());
                 request.getSession().setAttribute("keysize", keysize);
+
                 String nextJSP = "/rsafunctions.jsp";
+
+                if(pageName!=null && "rsasignverifyfunctions".equalsIgnoreCase(pageName))
+                {
+                    nextJSP = "/rsasignverifyfunctions.jsp";
+                }
+
+
                 RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
                 dispatcher.forward(request, response);
 
@@ -123,17 +133,238 @@ public class RSAFunctionality extends HttpServlet {
         String keysize = request.getParameter("keysize");
         String encryptdecryptparameter = request.getParameter("encryptdecryptparameter");
 
+        String signature = request.getParameter("signature");
 
-        //
+
+
 
 //        System.out.println("publiKeyParam " + publiKeyParam);
 //        System.out.println("privateKeParam " + privateKeParam);
-        // System.out.println("message " + message);
+//         System.out.println("message " + message);
 //        System.out.println("algo " + algo);
 //        System.out.println("keysize " + keysize);
 //        System.out.println("encryptdecryptparameter " + encryptdecryptparameter);
 //        System.out.println("methodName " + methodName);
+//
+//        System.out.println("signature " + signature);
 
+
+        if (METHOD_SIGN_VERIFY_TERSA.equalsIgnoreCase(methodName)) {
+
+            if (algo == null || algo.length() == 0) {
+                algo = "SHA256withRSA";
+            }
+
+
+
+             // This is Verification of the Message
+            if ("encrypt".equals(encryptdecryptparameter)) {
+
+                if (null == message || message.trim().length() == 0) {
+                    addHorizontalLine(out);
+                    out.println("<font size=\"2\" color=\"red\"> Message is Null or EMpty....</font>");
+                    return;
+
+                }
+
+                if (publiKeyParam != null && publiKeyParam.trim().length() > 0) {
+//                    publiKeyParam = publiKeyParam.replace("-----BEGIN PUBLIC KEY-----\n", "");
+//                    publiKeyParam = publiKeyParam.replace("-----END PUBLIC KEY-----", "");
+
+
+                    HttpPost post = null;
+
+                    try {
+
+
+                        Gson gson = new Gson();
+                        HttpClient client = HttpClientBuilder.create().build();
+                        String url1 = LoadPropertyFileFunctionality.getConfigProperty().get("ep") +  "rsa/verify";
+                        post = new HttpPost(url1);
+                        List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+                        urlParameters.add(new BasicNameValuePair("p_msg", message));
+                        urlParameters.add(new BasicNameValuePair("p_key", publiKeyParam));
+                        urlParameters.add(new BasicNameValuePair("p_algo", algo));
+                        urlParameters.add(new BasicNameValuePair("p_sig", signature));
+
+                        post.setEntity(new UrlEncodedFormEntity(urlParameters));
+                        post.addHeader("accept", "application/json");
+
+                        HttpResponse response1 = client.execute(post);
+
+                        if (response1.getStatusLine().getStatusCode() != 200) {
+                            if (response1.getStatusLine().getStatusCode() == 404) {
+                                BufferedReader br1 = new BufferedReader(
+                                        new InputStreamReader(
+                                                (response1.getEntity().getContent())
+                                        )
+                                );
+                                StringBuilder content1 = new StringBuilder();
+                                String line;
+                                while (null != (line = br1.readLine())) {
+                                    content1.append(line);
+                                }
+                                addHorizontalLine(out);
+                                out.println("<font size=\"4\" color=\"red\"> SYSTEM Error  " + content1 + "</font>");
+                                return;
+                            } else {
+                                addHorizontalLine(out);
+                                out.println("<font size=\"4\" color=\"red\"> SYSTEM Error Please Try Later If Problem Persist raise the feature request </font>");
+                                return;
+                            }
+
+                        }
+                        BufferedReader br1 = new BufferedReader(
+                                new InputStreamReader(
+                                        (response1.getEntity().getContent())
+                                )
+                        );
+                        StringBuilder content1 = new StringBuilder();
+                        String line;
+                        while (null != (line = br1.readLine())) {
+                            content1.append(line);
+                        }
+
+
+
+                        //System.out.println("line-- " + line);
+                        String ret = content1.toString();
+
+                        if ( ret.contains("Passed"))
+                        {
+                            out.println("<p><font size=\"4\" color=\"green\">" + ret + "</font></p>");
+                        }
+                        else {
+                            out.println("<p><font size=\"4\" color=\"red\">" + ret + "</font></p>");
+                        }
+
+                        return;
+
+
+                    } catch (Exception e) {
+                        addHorizontalLine(out);
+                        out.println("<font size=\"4\" color=\"red\"> " + e);
+                    }finally {
+                        if(post!=null)
+                        {
+                            post.releaseConnection();
+                        }
+                    }
+
+                } else {
+                    addHorizontalLine(out);
+                    out.println("<font size=\"2\" color=\"red\"> " + algo + " Public Key Can't be EMPTY </font>");
+
+                }
+            } else {
+
+                //Assumed Decrypt ...
+                // System.out.println(encryptdecryptparameter);
+                String encrypedmessagetextarea = request.getParameter("encrypedmessagetextarea");
+                //System.out.println("encrypedmessagetextarea ---> " + encrypedmessagetextarea);
+
+                if (privateKeParam != null && privateKeParam.trim().length() > 0) {
+
+                    boolean isBase64 = Base64.isArrayByteBase64(message.getBytes());
+                    if (!isBase64) {
+                        addHorizontalLine(out);
+                        out.println("<font size=\"3\" color=\"red\"> " + "Please Provide Base64 Encoded value Failed to Decrypt.. </font>");
+                        return;
+                    }
+
+                    if (null == message || message.trim().length() == 0) {
+                        addHorizontalLine(out);
+                        out.println("<font size=\"2\" color=\"red\"> RSA Encryped Message is Null or EMpty....</font>");
+                        return;
+
+                    }
+
+//                    privateKeParam = privateKeParam.replace("-----BEGIN PRIVATE KEY-----\n", "");
+//                    privateKeParam = privateKeParam.replace("-----END PRIVATE KEY-----", "");
+
+                    HttpPost post =null;
+
+                    try {
+
+                        byte[] content = privateKeParam.getBytes();
+                        InputStream is = new ByteArrayInputStream(content);
+                        InputStreamReader isr = new InputStreamReader(is);
+                        BufferedReader br = new BufferedReader(isr);
+
+                        Gson gson = new Gson();
+                        HttpClient client = HttpClientBuilder.create().build();
+                        String url1 = LoadPropertyFileFunctionality.getConfigProperty().get("ep") + "rsa/sign";
+                        post = new HttpPost(url1);
+                        List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+                        urlParameters.add(new BasicNameValuePair("p_msg", message));
+                        urlParameters.add(new BasicNameValuePair("p_key", privateKeParam));
+                        urlParameters.add(new BasicNameValuePair("p_algo", algo));
+
+                        post.setEntity(new UrlEncodedFormEntity(urlParameters));
+                        post.addHeader("accept", "application/json");
+
+                        HttpResponse response1 = client.execute(post);
+
+                        if (response1.getStatusLine().getStatusCode() != 200) {
+                            if (response1.getStatusLine().getStatusCode() == 404) {
+                                BufferedReader br1 = new BufferedReader(
+                                        new InputStreamReader(
+                                                (response1.getEntity().getContent())
+                                        )
+                                );
+                                StringBuilder content1 = new StringBuilder();
+                                String line;
+                                while (null != (line = br1.readLine())) {
+                                    content1.append(line);
+                                }
+                                addHorizontalLine(out);
+                                out.println("<font size=\"4\" color=\"red\"> SYSTEM Error  " + content1 + "</font>");
+                                return;
+                            } else {
+                                addHorizontalLine(out);
+                                out.println("<font size=\"4\" color=\"red\"> SYSTEM Error Please Try Later If Problem Persist raise the feature request </font>");
+                                return;
+                            }
+
+                        }
+                        BufferedReader br1 = new BufferedReader(
+                                new InputStreamReader(
+                                        (response1.getEntity().getContent())
+                                )
+                        );
+                        StringBuilder content1 = new StringBuilder();
+                        String line;
+                        while (null != (line = br1.readLine())) {
+                            content1.append(line);
+                        }
+
+
+
+
+                        addHorizontalLine(out);
+                        out.println("<textarea name=\"encrypedmessagetextarea\" class=\"form-control\" readonly=\"true\"  id=\"encrypedmessagetextarea\" rows=\"5\" cols=\"40\">" + content1.toString() + "</textarea>");
+                        return;
+
+
+                    } catch (Exception e) {
+                        addHorizontalLine(out);
+                        out.println("<font size=\"2\" color=\"red\"> " + e + "</font>");
+                    }finally {
+
+                        if(post!=null)
+                        {
+                            post.releaseConnection();
+                        }
+
+                    }
+
+
+                } else {
+                    addHorizontalLine(out);
+                    out.println("<font size=\"2\" color=\"red\"> " + algo + "  Key Can't be EMPTY </font>");
+                }
+            }
+        }
 
         if (METHOD_CALCULATERSA.equalsIgnoreCase(methodName)) {
 
