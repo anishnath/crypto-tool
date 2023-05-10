944b32a24ac80172b47fd0dc8a5045b7eee6bcb9
anish <anish@noreply.com>
2023-05-11 03:17:55 +0530

    Adding Blockchain Stuff's

--- src/main/java/z/y/x/r/LoadPropertyFileFunctionality.java
+++ src/main/java/z/y/x/r/LoadPropertyFileFunctionality.java
@@ -1,23 +1,22 @@
 package z.y.x.r;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Random;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 
 public class LoadPropertyFileFunctionality extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+
 	private static Properties prop = new Properties();
-	 
-	 protected String myParam = null; 
-	 
+
+	 protected String myParam = null;
+
 	 private static Map<String,String> configProperty = new HashMap<>();
 	  public void init(ServletConfig servletConfig) throws ServletException{
 
@@ -61,10 +60,26 @@ public class LoadPropertyFileFunctionality extends HttpServlet {
 			  configProperty.put("epn", "http://localhost/ntru/rest/");
 		  }
 
+		  if(prop.getProperty("sqlite")!=null)
+		  {
+			  configProperty.put("sqlite", prop.getProperty("sqlite"));
+		  }
+		  else{
+			  configProperty.put("sqlite", "/home/ec2-user/sqlite.db");
+		  }
+		  if(prop.getProperty("blockchain")!=null)
+		  {
+			  configProperty.put("blockchain", prop.getProperty("blockchain"));
+		  }
+		  else{
+			  configProperty.put("blockchain", "http://localhost:1888/");
+		  }
+
 
 		  System.out.println("API PORT-- " + prop.getProperty("port"));
 		  System.out.println("API URL-- " + prop.getProperty("url"));
 		  System.out.println("API ENDPOINT-- " + prop.getProperty("ep"));
+		  System.out.println("API BLOCKCHAIN-- " + prop.getProperty("blockchain"));
 
 
 	} catch (Exception e) {
@@ -72,12 +87,13 @@ public class LoadPropertyFileFunctionality extends HttpServlet {
 		  configProperty.put("url", "localhost");
 		  configProperty.put("ep", "http://localhost/crypto/rest/");
 		  configProperty.put("epn", "http://localhost/ntru/rest/");
+		  configProperty.put("blockchain", "http://localhost:1888/");
 
 
 
 	}
-      
-	    
+
+
 	  }
 
 	public static Map<String, String> getConfigProperty() {
