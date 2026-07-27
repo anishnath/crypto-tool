942b46a880d6231c8d0695927a4d1e4a2429f290
anish <0xnull@noreply.com>
2026-07-28 02:42:08 +0530

    Manic Race charts Update

--- src/main/webapp/manic/docs/searcher-09f2665d.js
+++ src/main/webapp/manic/docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-891a6e5e.js',
+                path_to_root + 'searchindex-566c6724.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
