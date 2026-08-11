c34dd771b4348f6eb0eaefd2fcec7c716201eaf2
anish <0xnull@noreply.com>
2026-08-12 00:19:58 +0530

    Manic Fixes  Updated

--- src/main/webapp/manic/docs/searcher-09f2665d.js
+++ src/main/webapp/manic/docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9fb17171.js',
+                path_to_root + 'searchindex-979a81e0.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
