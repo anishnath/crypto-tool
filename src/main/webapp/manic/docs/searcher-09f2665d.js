eed6b065db13e3db65a58adc93a36addc283fd5e
anish <0xnull@noreply.com>
2026-08-19 23:00:09 +0530

    Manic circuit kits

--- src/main/webapp/manic/docs/searcher-09f2665d.js
+++ src/main/webapp/manic/docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9fe11d60.js',
+                path_to_root + 'searchindex-b6574f79.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
