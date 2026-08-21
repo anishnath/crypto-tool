d6c0149c844c8fa02b1da1d6f16ab0bd69262e25
anish <0xnull@noreply.com>
2026-08-22 01:54:52 +0530

    Manic Chemistry Onboarding

--- src/main/webapp/manic/docs/searcher-09f2665d.js
+++ src/main/webapp/manic/docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8f1d47f8.js',
+                path_to_root + 'searchindex-31b9aac3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
